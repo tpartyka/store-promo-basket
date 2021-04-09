1896ccbc6974466041f1e829ef79e612600be98d
tomaszpartyka <tomasz.partyka@tesco.com>
2021-04-09 09:49:55 +0200

    Cleanup

--- src/main/scala/domain/model/BasketWithRewards.scala
+++ src/main/scala/domain/model/BasketWithRewards.scala
@@ -1,3 +1,3 @@
 package domain.model
 
-case class BasketWithRewards(products: Basket, rewards: Map[Index, (String, RewardType)])
+case class BasketWithRewards(products: Basket, rewards: AppliedRewards)
